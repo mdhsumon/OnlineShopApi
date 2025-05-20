a609138377037c1e9988cc9be05e829d37ae1972
Delower Hossain <mdh.sumon@gmail.com>
2025-05-20 23:21:14 +0600

    Initialization

---
+++ src/test/java/com/onlineshopapi/OnlineShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlineshopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
