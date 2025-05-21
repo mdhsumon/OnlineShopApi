af7494bee06e2008040400b760b8fffc749b06fa
Delower Hossain <mdh.sumon@gmail.com>
2025-05-21 13:13:14 +0600

    Lombok error fix

--- src/main/java/com/onlineshopapi/entities/ProductEntity.java
+++ src/main/java/com/onlineshopapi/entities/ProductEntity.java
@@ -1,7 +1,11 @@
 package com.onlineshopapi.entities;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "products")
 public class ProductEntity {
@@ -11,36 +15,4 @@ public class ProductEntity {
     private String name;
     private String description;
     private Double price;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
 }
